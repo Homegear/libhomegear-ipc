9f34fb549f867988a640c9deb800ac7a093b7689
Dr. Sathya Laufer <s.laufer@homegear.email>
2020-08-10 16:55:22 +0200

    Added UI notification methods

--- src/IIpcClient.h
+++ src/IIpcClient.h
@@ -50,82 +50,81 @@
 #include <unordered_map>
 #include <functional>
 
-namespace Ipc
-{
+namespace Ipc {
 
-class IIpcClient : public IQueue
-{
-public:
-	explicit IIpcClient(std::string socketPath);
-	virtual ~IIpcClient();
-	virtual void dispose();
+class IIpcClient : public IQueue {
+ public:
+  explicit IIpcClient(std::string socketPath);
+  virtual ~IIpcClient();
+  virtual void dispose();
 
-	static std::string version();
-    bool connected() { return !_closed; }
-	PVariable invoke(const std::string& methodName, const PArray& parameters, int32_t timeout = 0);
-    virtual void start();
-	virtual void start(size_t processingThreadCount);
-	virtual void stop();
-protected:
-	struct RequestInfo
-	{
-		std::mutex waitMutex;
-		std::condition_variable conditionVariable;
-	};
-	typedef std::shared_ptr<RequestInfo> PRequestInfo;
+  static std::string version();
+  bool connected() { return !_closed; }
+  PVariable invoke(const std::string &methodName, const PArray &parameters, int32_t timeout = 0);
+  virtual void start();
+  virtual void start(size_t processingThreadCount);
+  virtual void stop();
+ protected:
+  struct RequestInfo {
+    std::mutex waitMutex;
+    std::condition_variable conditionVariable;
+  };
+  typedef std::shared_ptr<RequestInfo> PRequestInfo;
 
-	class QueueEntry : public IQueueEntry
-	{
-	public:
-		QueueEntry() = default;
-		explicit QueueEntry(std::vector<char>& packet) { this->packet = packet; }
-		virtual ~QueueEntry() = default;
+  class QueueEntry : public IQueueEntry {
+   public:
+    QueueEntry() = default;
+    explicit QueueEntry(std::vector<char> &packet) { this->packet = packet; }
+    virtual ~QueueEntry() = default;
 
-		std::vector<char> packet;
-	};
+    std::vector<char> packet;
+  };
 
-	std::mutex _disposeMutex;
-	bool _disposing = false;
-	std::string _socketPath;
-	int32_t _fileDescriptor = -1;
-	int64_t _lastGargabeCollection = 0;
-	std::atomic_bool _stopped{true};
-	std::atomic_bool _closed{true};
-	std::mutex _sendMutex;
-	std::mutex _rpcResponsesMutex;
-	std::unordered_map<pthread_t, std::unordered_map<int32_t, PIpcResponse>> _rpcResponses;
-	std::map<std::string, std::function<PVariable(PArray& parameters)>> _localRpcMethods;
-	std::thread _mainThread;
-	std::thread _maintenanceThread;
-	std::mutex _requestInfoMutex;
-	std::map<pthread_t, PRequestInfo> _requestInfo;
-	std::mutex _packetIdMutex;
-	int32_t _currentPacketId = 0;
+  std::mutex _disposeMutex;
+  bool _disposing = false;
+  std::string _socketPath;
+  int32_t _fileDescriptor = -1;
+  int64_t _lastGargabeCollection = 0;
+  std::atomic_bool _stopped{true};
+  std::atomic_bool _closed{true};
+  std::mutex _sendMutex;
+  std::mutex _rpcResponsesMutex;
+  std::unordered_map<pthread_t, std::unordered_map<int32_t, PIpcResponse>> _rpcResponses;
+  std::map<std::string, std::function<PVariable(PArray &parameters)>> _localRpcMethods;
+  std::thread _mainThread;
+  std::thread _maintenanceThread;
+  std::mutex _requestInfoMutex;
+  std::map<pthread_t, PRequestInfo> _requestInfo;
+  std::mutex _packetIdMutex;
+  int32_t _currentPacketId = 0;
 
-	std::unique_ptr<BinaryRpc> _binaryRpc;
-	std::unique_ptr<RpcDecoder> _rpcDecoder;
-	std::unique_ptr<RpcEncoder> _rpcEncoder;
+  std::unique_ptr<BinaryRpc> _binaryRpc;
+  std::unique_ptr<RpcDecoder> _rpcDecoder;
+  std::unique_ptr<RpcEncoder> _rpcEncoder;
 
-	void init();
-	void connect();
-	void mainThread();
-	void sendResponse(PVariable packetId, PVariable variable);
+  void init();
+  void connect();
+  void mainThread();
+  void sendResponse(PVariable packetId, PVariable variable);
 
-	void processQueueEntry(int32_t index, std::shared_ptr<IQueueEntry>& entry) override;
-	PVariable send(std::vector<char>& data);
+  void processQueueEntry(int32_t index, std::shared_ptr<IQueueEntry> &entry) override;
+  PVariable send(std::vector<char> &data);
 
-	virtual void onConnect() = 0;
-	virtual void onConnectError() {};
-    virtual void onDisconnect() {};
+  virtual void onConnect() = 0;
+  virtual void onConnectError() {};
+  virtual void onDisconnect() {};
 
-	// {{{ RPC methods
-        virtual Ipc::PVariable ping(Ipc::PArray& parameters) { return std::make_shared<Ipc::Variable>(); }
-		virtual Ipc::PVariable broadcastEvent(Ipc::PArray& parameters) { return std::make_shared<Ipc::Variable>(); }
-		virtual Ipc::PVariable broadcastNewDevices(Ipc::PArray& parameters) { return std::make_shared<Ipc::Variable>(); }
-		virtual Ipc::PVariable broadcastDeleteDevices(Ipc::PArray& parameters) { return std::make_shared<Ipc::Variable>(); }
-		virtual Ipc::PVariable broadcastUpdateDevice(Ipc::PArray& parameters) { return std::make_shared<Ipc::Variable>(); }
-        virtual Ipc::PVariable broadcastVariableProfileStateChanged(Ipc::PArray& parameters) { return std::make_shared<Ipc::Variable>(); }
-	// }}}
+  // {{{ RPC methods
+  virtual Ipc::PVariable ping(Ipc::PArray &parameters) { return std::make_shared<Ipc::Variable>(); }
+  virtual Ipc::PVariable broadcastEvent(Ipc::PArray &parameters) { return std::make_shared<Ipc::Variable>(); }
+  virtual Ipc::PVariable broadcastNewDevices(Ipc::PArray &parameters) { return std::make_shared<Ipc::Variable>(); }
+  virtual Ipc::PVariable broadcastDeleteDevices(Ipc::PArray &parameters) { return std::make_shared<Ipc::Variable>(); }
+  virtual Ipc::PVariable broadcastUpdateDevice(Ipc::PArray &parameters) { return std::make_shared<Ipc::Variable>(); }
+  virtual Ipc::PVariable broadcastVariableProfileStateChanged(Ipc::PArray &parameters) { return std::make_shared<Ipc::Variable>(); }
+  virtual Ipc::PVariable broadcastUiNotificationCreated(Ipc::PArray &parameters) { return std::make_shared<Ipc::Variable>(); }
+  virtual Ipc::PVariable broadcastUiNotificationRemoved(Ipc::PArray &parameters) { return std::make_shared<Ipc::Variable>(); }
+  virtual Ipc::PVariable broadcastUiNotificationAction(Ipc::PArray &parameters) { return std::make_shared<Ipc::Variable>(); }
+  // }}}
 };
 
 }
